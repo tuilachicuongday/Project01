3fe01d71bbce6a1abfd88ffaa6831c81a025d0c8
tartarus2000 <101467919+tartarus2000@users.noreply.github.com>
2022-03-12 22:40:19 +0700

    Update Program.cs

--- Project01/Project01/Program.cs
+++ Project01/Project01/Program.cs
@@ -7,7 +7,6 @@ namespace Project01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello My name is Monkey D.C!");
-            Console.WriteLine("Hello My name is Phung Trong Khang");
             Console.WriteLine("Hello My name is Vo Ngoc An");
         }
     }
